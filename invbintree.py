b87ea07ab9ef4ed9001a3aa2e52d394c39ad188f
Harsha Bharadwaj <88173012+harsha-bharadwaj100@users.noreply.github.com>
2025-02-14 22:13:23 +0530

    implement solutions for binary tree inversion and construction from preorder and inorder traversal

---
+++ invbintree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from typing import Optional
+
+
+class Solution:
+    def invertTree(self, root):
+        if not root:
+            return None
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
+
+
+sol = Solution()
+print(sol.invertTree([4, 2, 7, 1, 3, 6, 9]))
